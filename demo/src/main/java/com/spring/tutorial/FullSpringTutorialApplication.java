e7dffb642c2ead969d915da4b5d9a4efb772d9ce
Johndavidcabal <Johndavidcabal000@gmail.com>
2022-02-24 22:02:55 -0400

    comit

---
+++ demo/src/main/java/com/spring/tutorial/FullSpringTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.spring.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullSpringTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullSpringTutorialApplication.class, args);
+	}
+
+}
