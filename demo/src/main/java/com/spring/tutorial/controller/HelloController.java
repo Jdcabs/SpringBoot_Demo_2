89e64d73ad06b92cdca8d5a178f3d677064c6744
Johndavidcabal <Johndavidcabal000@gmail.com>
2022-02-25 09:35:31 -0400

    commit

--- demo/src/main/java/com/spring/tutorial/controller/HelloController.java
+++ demo/src/main/java/com/spring/tutorial/controller/HelloController.java
@@ -1,8 +1,14 @@
 package com.spring.tutorial.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello There";
+    }
     
 }
