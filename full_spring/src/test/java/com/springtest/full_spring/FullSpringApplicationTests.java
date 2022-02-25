93d8abddfa7fabbea36c7ea54fff51d9c1f9c4fb
Johndavidcabal <Johndavidcabal000@gmail.com>
2022-02-24 21:25:50 -0400

    commit

---
+++ full_spring/src/test/java/com/springtest/full_spring/FullSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtest.full_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
